c4683a728810e8381d0911c5da70717bc09dc2f0
gavin-gallant <gavin.gallant@mths.ca>
2024-10-22 11:06:54 -0400

    This program makes 2 numbers and compares them

--- main.ts
+++ main.ts
@@ -1,8 +1,45 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Gavin Gallant
+ * Created on: oct 2024
+ * This program makes 2 numbers and compares them
 */
 
-basic.showString('Hello, World!')
+// variables
+let numberA: number = 0
+let numberB: number = 0
+let score: number = 0
+numberA = randint(0, 99)
+numberB = randint(0, 99)
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onGesture(Gesture.Shake, function () {
+    if (numberA < numberB)
+    {
+        basic.clearScreen()
+        basic.showString(numberA + "<" + numberB)
+        basic.showIcon(IconNames.Sad)
+    }
+    else
+    {
+        basic.clearScreen()
+        basic.showString(numberA + ">" + numberB)
+        basic.showIcon(IconNames.Sad)
+    }
+
+
+   
+})
+
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showString("#1:" + numberA )
+    basic.showIcon(IconNames.Happy)
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+    basic.showString("#2:" + numberB)
+    basic.showIcon(IconNames.Happy)
+})
