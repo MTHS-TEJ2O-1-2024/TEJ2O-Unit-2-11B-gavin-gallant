1f53025eeda81d82cd30b2aac1667e46889def99
gavin-gallant <gavin.gallant@mths.ca>
2024-10-23 15:25:05 +0000

    fixed

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -10,31 +10,30 @@ import random
 number_a = random.randint(1, 99)
 number_b = random.randint(1, 99)
 
-#on shake compare the numbers
+# on shake compare the numbers
 while True:
     if accelerometer.was_gesture("shake"):
         if number_a < number_b:
             display.clear()
-            display.scroll (number_a + "<" + number_b)
+            display.scroll(number_a + "<" + number_b)
             display.show(Image.SAD)
             sleep(1000)
 
         else:
-            display.display.scroll (number_a + ">" + number_b)
+            display.display.scroll(number_a + ">" + number_b)
             display.clear()
             display.show(Image.SAD)
             sleep(1000)
         display.clear()
 
-#on button a pressed
+    # on button a pressed
 
     if button_a.is_pressed():
         display.clear()
         display.scroll("#1:" + number_a)
 
-#on btton b pressed
+    # on btton b pressed
 
     if button_b.is_pressed():
         display.clear()
         display.scroll("#2:" + number_b)
-
