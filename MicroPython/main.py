31ae9d236219b0261599a64624ce21952fc52b9a
gavin-gallant <gavin.gallant@mths.ca>
2024-11-21 16:25:57 +0000

    still eorr split lines of code in to multiple line 18 2537

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -9,18 +9,22 @@ import random
 
 number_a = random.randint(1, 99)
 number_b = random.randint(1, 99)
-
+display.show(Image.HAPPY)
 # on shake compare the numbers
 while True:
     if accelerometer.was_gesture("shake"):
         if number_a < number_b:
             display.clear()
-            display.scroll(number_a + "<" + number_b)
+            display.scroll(number_a )
+            display.scroll("<")
+            display.scroll(number_b)
             display.show(Image.SAD)
             sleep(1000)
 
         else:
-            display.display.scroll(number_a + ">" + number_b)
+            display.display.scroll(number_a)
+            display.display.scroll(">")
+            display.display.scroll(number_b)
             display.clear()
             display.show(Image.SAD)
             sleep(1000)
@@ -30,10 +34,12 @@ while True:
 
     if button_a.is_pressed():
         display.clear()
-        display.scroll("#1:" + number_a)
+        display.scroll("#1:")
+        display.scroll(number_a)
 
     # on btton b pressed
 
     if button_b.is_pressed():
         display.clear()
-        display.scroll("#2:" + number_b)
+        display.scroll("#2:")
+        display.scroll(number_b)
